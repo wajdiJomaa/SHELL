afe59c2f5dceeeceb9ed60bc3f421274a3049b87
wajdi jomaa <blackwajdi9@gmail.com>
2026-01-10 17:35:47 +0200

    add echo command

--- app/main.py
+++ app/main.py
@@ -6,15 +6,41 @@ def main():
         sys.stdout.write("$ ")
         try:
             command = input()
-
-            if command.strip() == "exit":
-                sys.exit(0)
-
-
         except EOFError:
             sys.exit()
         
-        print(f"{command}: command not found")
+        scanned_command = scan(command.strip())
+
+        if scanned_command[0] == "exit":
+            sys.exit(0)
+        elif scanned_command[0] == "echo":
+            for i in range (1, len(scanned_command)):
+                print(scanned_command[i], end=" ")
+            
+            print()
+        else:
+            print(f"{command}: command not found")
+
+
+
+def scan(command):
+    current = 0
+    result = []
+    
+    while current < len(command):
+        match command[current]:
+            case " ":
+                current += 1
+            case _:
+                s = ""
+                while(current < len(command) and command[current] != " "):
+                    s += command[current]
+                    current += 1
+                result.append(s)
+
+    return result
+
+
 
 if __name__ == "__main__":
     main()
