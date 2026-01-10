549b2fd4862a56e8cb878f8825784ce6a48c9328
wajdi jomaa <blackwajdi9@gmail.com>
2026-01-10 17:14:00 +0200

    add exit command

--- app/main.py
+++ app/main.py
@@ -6,8 +6,14 @@ def main():
         sys.stdout.write("$ ")
         try:
             command = input()
+
+            if command.strip() == "exit":
+                sys.exit(0)
+
+
         except EOFError:
-            break
+            sys.exit()
+        
         print(f"{command}: command not found")
 
 if __name__ == "__main__":
