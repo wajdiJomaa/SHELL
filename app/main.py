61405f8f416456d12f5b18774dcc310e8a1a377d
wajdi jomaa <blackwajdi9@gmail.com>
2026-01-13 16:39:52 +0200

    add ~ support + fix

--- app/main.py
+++ app/main.py
@@ -98,7 +98,7 @@ class SHELL:
         
         if index == 0 and scanned_command[1].startswith("~"):
             current_dir = os.path.expanduser("~")
-
+            return
         scanned_command[1] = scanned_command[1][index:]
         if len(scanned_command) < 2:
             os.chdir(os.path.expanduser("~"))
