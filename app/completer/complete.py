a980c55e469c71cdc4f051e265372fe367fe209b
wajdiJomaa <blackwajdi9@gmail.com>
2026-01-25 23:09:51 +0200

    add support for path completions

--- app/completer/complete.py
+++ app/completer/complete.py
@@ -1,15 +1,34 @@
 from app.builtins import built_ins
+import os
+
+def built_in_complete(text):
+    return [built_in + " " for built_in in built_ins if built_in.startswith(text)]
+
+def complete_from_path(text):
+    completions = []
+    paths = os.environ.get("PATH", "").split(os.pathsep)
+    for path in paths:
+        if os.path.isdir(path) is False:
+            continue
+
+        for file in os.listdir(path):
+            if file.startswith(text):
+                completions.append(file + " ")
+    
+    return completions
+
 
 completions = []
 
 def complete(text, state):
     global completions
     if state == 0:
-        completions = [built_in + " " for built_in in built_ins if built_in.startswith(text)]
+        completions = built_in_complete(text)
+        completions.extend(complete_from_path(text))
+    
     if state < len(completions):
         return completions[state]
-    else:
-        completions = []
-        return None
+   
+    return None
 
     
